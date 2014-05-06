22e03427e6e51bb711b96a58bad4ae1b5af3a00b
unknown <jirikolapa@gmail.com>
2014-05-06 21:37:27 +0800

    try

--- app/views/client/partials/_searchbar.blade.php
+++ app/views/client/partials/_searchbar.blade.php
@@ -2,9 +2,9 @@
 				<div class="zone-search zone clearfix">
 					<div class="search-container container-24">
 						<div class="search block">
-							<form id="default-search" class="default-search clearfix">
-								<input type="text" id="search-what" class="text-input-black input-text" placeholder="What are you looking for?" />
-								<input type="text" class="text-input-black input-text" placeholder="Where are you looking for it?" />
+								{{ Form::open(array('action' => 'BusinessesController@search','id' =>'default-search', 'class' => 'default-search clearfix')) }}
+								<input name="category" type="text" id="search-what" class="text-input-black input-text" placeholder="What are you looking for?" />
+								<input name="text"  type="text" class="text-input-black input-text" placeholder="Where are you looking for it?" />
 								<select id="category-selector-default" name="category-default" tabindex="1">
 									<option value="">- Select Category -</option>
 									<option value="airport">Airport</option>
