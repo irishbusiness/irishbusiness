3e2cf95935ec40889b69035cb3e86a84d6340a8b
unknown <dexterb2992@gmail.com>
2014-05-26 11:34:03 +0800

    modified the registration form

--- app/views/admin/admin_register.blade.php
+++ app/views/admin/admin_register.blade.php
@@ -11,35 +11,35 @@
                 {{ Form::label('firstname', "Firstname", ["class"=> "text-colorful"]) }}
                 <br>
                 {{ Form::text('firstname', '', ["class"=>"text-input-grey full",
-                    "placeholder"=>"John"]) }}
+                    "placeholder"=>"Firstname"]) }}
                 {{$errors->first('firstname','<span class="error">:message</span>')}}
                 </div>
                 <div class="form-group">
                     {{ Form::label('lastname', "Lastname", ["class"=> "text-colorful"]) }}
                     <br>
                     {{ Form::text('lastname', '', ["class"=>"text-input-grey full",
-                        "placeholder"=>"Doe"]) }}
+                        "placeholder"=>"Lastname"]) }}
                     {{$errors->first('lastname','<span class="error">:message</span>')}}
                 </div>
                 <div class="form-group">
                     {{ Form::label('email', "Email", ["class"=> "text-colorful"]) }}
                     <br>
                     {{ Form::text('email', '', ["class"=>"text-input-grey full",
-                        "placeholder"=>"johndoe@example.com"]) }}
+                        "placeholder"=>"your_email@example.com"]) }}
                     {{$errors->first('email','<span class="error">:message</span>')}}
                 </div>
                 <div class="form-group">
                     {{ Form::label('username', "Username", ["class"=> "text-colorful"]) }}
                     <br>
                     {{ Form::text('username', '', ["class"=>"text-input-grey full",
-                        "placeholder"=>"johndoe123"]) }}
+                        "placeholder"=>"Username"]) }}
                     {{$errors->first('username','<span class="error">:message</span>')}}
                 </div>
                 <div class="form-group">
                     {{ Form::label('password', "Password", ["class"=> "text-colorful"]) }}
                     <br>
                     {{ Form::password('password', ["class"=>"text-input-grey full",
-                        "placeholder"=>"johndoe123456789"]) }}
+                        "placeholder"=>"*********"]) }}
                     {{$errors->first('password','<span class="error">:message</span>')}}
                 </div>
                 <div class="form-group align-right">
