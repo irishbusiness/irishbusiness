409182b48ac1018e7d3722cfacb64978b08c1317
Zemiel <lourdrivera123@gmail.com>
2014-06-06 15:32:36 +0800

    add softdelete to reviews migration

--- app/views/client/layouts/notes.php
+++ app/views/client/layouts/notes.php
@@ -1,15 +1,5 @@
 /*
-what we need:
 
-//blog header
-//blog title
-//blog body (this is the wysiwyg editor)
-//blog posted
-//social link (facebook, google+, twitter, linkedin)
-*/
-
-/*
-what we need to do:
-create a slug and it must be the title when the use clicks on the read more
+on the real world remove business seeder because it might have conflict
 
 */
