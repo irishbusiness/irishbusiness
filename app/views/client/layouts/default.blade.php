d54fffb2c8033b4592aeca4f60324ec2819108fd
unknown <jirikolapa@gmail.com>
2014-06-11 17:38:39 +0800

    all

--- app/views/client/layouts/default.blade.php
+++ app/views/client/layouts/default.blade.php
@@ -6,7 +6,8 @@
 		<meta charset="utf-8">
         @yield('title')
         
-		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no" />
+		<!-- <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no" --> />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         {{ HTML::script('scripts/jquery-1.10.2.min.js') }}
 
         @yield('linksfirst')
