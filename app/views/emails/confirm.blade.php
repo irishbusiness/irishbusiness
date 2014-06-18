d2d67e5af23638143feeb79a57d16e6a2d81f465
unknown <jirikolapa@gmail.com>
2014-06-18 23:15:44 +0800

    emails

--- app/views/emails/confirm.blade.php
+++ app/views/emails/confirm.blade.php
@@ -8,7 +8,8 @@
 <body>
 	<p> Hi {{UCfirst($firstname)}}, 
 	<br>	
-	<p >Please confirm your email by clicking on this<a href="{{'http://teamlaravel.com/register/activate/' . $token }}"> link. </a></p>
+<!-- 	<p >Please confirm your email by clicking on this<a href="{{'http://teamlaravel.com/register/activate/' . $token }}"> link. </a></p> -->
+	<p >Please confirm your email by clicking on this <a href="{{URL::to('register/activate/', array($token)) }}"> link. </a></p>
 	<p >Thank you.</p>
 
 
