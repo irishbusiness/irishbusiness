2ec00bceccf24172f547a26beec9a2b9900ba78b
unknown <dexterb2992@gmail.com>
2014-06-27 18:41:10 +0800

    fix searchresults

--- app/views/client/partials/_sidebar.blade.php
+++ app/views/client/partials/_sidebar.blade.php
@@ -12,13 +12,17 @@
 						<a href="{{ URL::to($recentcompany->branches->first()->branchslug) }}" class="thumbnail">
 							<img src="{{ ($recentcompany->logo == '') ? URL::asset('images/image-not-available.png') : URL::asset($recentcompany->logo) }}" alt="" />
 						</a>
-						<a href="{{ URL::to($recentcompany->branches->first()->branchslug) }}" class="entry-title">{{ decode($recentcompany->name) }}</a>
-						<div class="entry-excerpt">{{ Str::limit(decode($recentcompany->business_description), 50) }}</div>
+						<a href="{{ URL::to($recentcompany->branches->first()->branchslug) }}" class="entry-title">
+							{{ decode($recentcompany->name) }}
+						</a>
+						<div class="entry-excerpt">
+							{{ Str::limit(decode($recentcompany->business_description), 50) }}
+						</div>
 					</li>
 				@endif	
 			@endforeach
 		</ul>
-	</div>
+	<!-- </div> -->
 	<div class="latest-news block">
 		<div class="block-title">
 			<h3>Latest News</h3>
@@ -29,8 +33,12 @@
 				<a href="{{ URL::to('/blog/'.$recentblog->slug) }}" class="thumbnail">
 					<img src="{{ ($recentblog->blogheaderimage == '') ? URL::asset('images/no_photo_available.jpg') : URL::asset($recentblog->blogheaderimage) }}" alt="" />
 				</a>
-				<a href="{{ URL::to('/blog/'.$recentblog->slug) }}" class="entry-title">{{ decode($recentblog->title) }}</a>
-				<div class="entry-excerpt">{{ Str::limit(strip_tags(decode($recentblog->body)) , 50) }}</div>
+				<a href="{{ URL::to('/blog/'.$recentblog->slug) }}" class="entry-title">
+					{{ decode($recentblog->title) }}
+				</a>
+				<div class="entry-excerpt">
+					{{ Str::limit(strip_tags(decode($recentblog->body)) , 50) }}
+				</div>
 			</li>
 		@endforeach
 		</ul>
