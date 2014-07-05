066e7a5b08dd5548433dbaa4d5a36f8c58a22f25
Zem <lourdrivera123@gmail.com>
2014-07-05 20:45:25 +0800

    clean ug blog controller

--- app/IrishBusiness/Repositories/BlogRepository.php
+++ app/IrishBusiness/Repositories/BlogRepository.php
@@ -1,5 +1,133 @@
 <?php namespace IrishBusiness\Repositories;
 
+use Blog;
+use Auth;
+
 class BlogRepository {
 
+	function getBlog($id)
+	{
+        $blog = Blog::where('slug', '=', $id)->first();
+
+		return $blog;
+	}
+	
+	function getBlogById($id)
+	{
+        $blog = Blog::findOrFail($id);
+
+		return $blog;
+	}
+
+	function getAll()
+	{
+        $blogs = Blog::orderBy('created_at', 'desc')->get();
+
+		return $blogs;
+	}
+
+	function create($input)
+	{
+        $blog = new Blog;
+        $blog->title = $input['title'];
+        $blog->body = $input['content'];
+        $blog->business_id  =   Auth::user()->user()->business->id;
+        // files storage folder
+        $dir = public_path().'/images/blog/';
+        if($input['blogurl'] == null){
+            $title = stripcslashes(strtolower($input['title']));
+            $tempSlug = trim(preg_replace("/[\']/", "", $title));
+            $blog->slug = strtolower(preg_replace("/[\s_\']/", "-", $tempSlug));
+        } else {
+            $blog->slug = strtolower($input['blogurl']);
+        }
+
+        //check if the file isset
+        if( !is_null($input['blogheaderimage']))
+        {
+            $image  =   $input['blogheaderimage'];
+            $imagename = md5(date('YmdHis')).'.jpg';
+            $filename = $dir.$imagename;
+
+            if ($image->getMimeType() == 'image/png'
+                || $image->getMimeType() == 'image/jpg'
+                || $image->getMimeType() == 'image/gif'
+                || $image->getMimeType() == 'image/jpeg'
+                || $image->getMimeType() == 'image/pjpeg')
+            {
+                $image->move($dir, $filename);
+                $blog->blogheaderimage  =   'images/blog/'.$imagename;
+            } else {
+                $blog->blogheaderimage  =   'images/blog/'.$imagename;
+            }
+
+        } else {
+            $blog->blogheaderimage  =   '';
+        }
+
+        $blog->save();
+
+        return $blog;
+	}
+
+	function update($id)
+	{
+		// files storage folder
+        $dir = public_path().'/images/blog/';
+        $image = Input::file('blogheaderimage');
+
+        $blog = Blog::findOrFail($id);
+        $blog->title = Input::get('title');
+        $blog->body = Input::get('content');
+        $blog->facebook = Input::get('facebook');
+        $blog->google = Input::get('google');
+        $blog->twitter = Input::get('twitter');
+        $blog->linkedin = Input::get('linkedin');
+        $blog->business_id  =   Auth::user()->user()->business->id;
+        // $blog->business_id = 2;
+
+        if(Input::get('blogurl') == null){
+            $title = stripcslashes(strtolower(Input::get('title')));
+            $tempSlug = trim(preg_replace("/[\']/", "", $title));
+            $blog->slug = strtolower(preg_replace("/[\s_\']/", "-", $tempSlug));
+        } else {
+            $blog->slug = strtolower(Input::get('blogurl'));
+        }
+
+        //check if the file isset
+        if( Input::hasFile('blogheaderimageedit'))
+        {
+            $image  =   Input::file('blogheaderimageedit');
+            $imagename = md5(date('YmdHis')).'.jpg';
+            $filename = $dir.$imagename;
+
+            if ($image->getMimeType() == 'image/png'
+                || $image->getMimeType() == 'image/jpg'
+                || $image->getMimeType() == 'image/gif'
+                || $image->getMimeType() == 'image/jpeg'
+                || $image->getMimeType() == 'image/pjpeg')
+            {
+                $image->move($dir, $filename);
+                // unlink(public_path().'/'.$blog->blogheaderimage);
+
+                $blog->blogheaderimage  =   'images/blog/'.$imagename;
+            } else {
+                $blog->blogheaderimage  =   'images/blog/'.$imagename;
+            }
+
+        }
+
+        $blog->save();
+
+        return $blog;
+	}
+
+	function getDecodedBlogById($id)
+	{
+        $blog = Blog::findOrFail($id);
+
+        $blog->body = html_entity_decode(stripcslashes($blog->body));
+
+        return $blog;
+	}
 }
--- app/controllers/BlogController.php
+++ app/controllers/BlogController.php
@@ -1,27 +1,33 @@
 <?php
 
+use IrishBusiness\Repositories\BlogRepository;
+
 class BlogController extends \BaseController {
 
-    public function __contruct() {
+    protected $blog;
+
+    function __construct(BlogRepository $blog) {
+        $this->blog = $blog;
         $this->beforeFilter('csrf', ['on' => 'post']);
     }
 
     public function show($slug, $id)
     {
-        $blog = Blog::where('slug', '=', $id)->first();
+        $blog = $this->blog->getBlog($id);
+
         return View::make('client.blogpost', compact('blog'));
-       
     }
 
     public function edit($id)
     {
-        $blog = Blog::whereSlug($id)->first();
+        $blog = $this->blog->getBlog($id);
+
         return View::make('client.editblogpost', compact('blog'))->with("title", "Client - Edit Blog");
     }
 
     public function destroy($id)
     {
-        $blog = Blog::findOrFail($id);
+        $blog = $this->blog->getBlogById($id);
         $blog->delete();
 
         return Redirect::to('/blog');
@@ -29,7 +35,8 @@ class BlogController extends \BaseController {
 
     public function index()
     {
-        $blogs = Blog::orderBy('created_at', 'desc')->get();
+        $blogs = $this->blog->getAll();
+
         return View::make('client.bloglist', compact('blogs'))->with("title", "Blogs");
     }
 
@@ -38,230 +45,39 @@ class BlogController extends \BaseController {
         return View::make('admin.admin_manage_blog')->with("title", "Admin - Manage Blogs");
     }
 
-    public function add(){
+    public function add()
+    {
         return View::make('client.companytabs.addblog')->with('title', 'Add Blog');
     }
 
-    public function manageblog(){
-        $blogs = Blog::orderBy('created_at', 'desc')->get();
+    public function manageblog()
+    {
+        $blogs = $this->blog->getAll();
+
         return View::make('admin.admin_manage_blog')->with('blogs', $blogs)->with('title', 'Admin - Manage Blogs');
     }
 
     public function store()
     {
-        // files storage folder
-        $dir = public_path().'/images/blog/';
+        $blog = $this->blog->create(Input::all());
 
-        $blog = new Blog;
-        $blog->title = Input::get('title');
-        $blog->body = Input::get('content');
-        // $blog->facebook = Input::get('facebook');
-        // $blog->google = Input::get('google');
-        // $blog->twitter = Input::get('twitter');
-        // $blog->linkedin = Input::get('linkedin');
-        $blog->business_id  =   Auth::user()->user()->business->id;
-        
-        if(Input::get('blogurl') == null){
-            $title = stripcslashes(strtolower(Input::get('title')));
-            $tempSlug = trim(preg_replace("/[\']/", "", $title));
-            $blog->slug = strtolower(preg_replace("/[\s_\']/", "-", $tempSlug));
-        } else {
-            $blog->slug = strtolower(Input::get('blogurl'));
-        }
-        // $blog->business_id = Auth::user()->user()->id;
-
-        //check if the file isset
-        if( Input::hasFile('blogheaderimage'))
-        {
-            $image  =   Input::file('blogheaderimage');
-            $imagename = md5(date('YmdHis')).'.jpg';
-            $filename = $dir.$imagename;
-
-            if ($image->getMimeType() == 'image/png'
-                || $image->getMimeType() == 'image/jpg'
-                || $image->getMimeType() == 'image/gif'
-                || $image->getMimeType() == 'image/jpeg'
-                || $image->getMimeType() == 'image/pjpeg')
-            {
-                $image->move($dir, $filename);
-                $blog->blogheaderimage  =   'images/blog/'.$imagename;
-            } else {
-                $blog->blogheaderimage  =   'images/blog/'.$imagename;
-            }
-
-        } else {
-            $blog->blogheaderimage  =   '';
-        }
-
-        $blog->save();
-
-//        return stripslashes(Input::get('content'));
-        return Redirect::to('/blog/'.$blog->slug);
+        return Redirect::to(Auth::user()->user()->business->branches()->first()->branchslug.'/blog/'.$blog->slug);
     }
 
     public function update($id)
     {
-        // files storage folder
-        $dir = public_path().'/images/blog/';
-        $image = Input::file('blogheaderimage');
+        $blog = $this->blog->update($id);
 
-        $blog = Blog::findOrFail($id);
-        $blog->title = Input::get('title');
-        $blog->body = Input::get('content');
-        $blog->facebook = Input::get('facebook');
-        $blog->google = Input::get('google');
-        $blog->twitter = Input::get('twitter');
-        $blog->linkedin = Input::get('linkedin');
-        $blog->business_id  =   Auth::user()->user()->business->id;
-        // $blog->business_id = 2;
-
-        if(Input::get('blogurl') == null){
-            $title = stripcslashes(strtolower(Input::get('title')));
-            $tempSlug = trim(preg_replace("/[\']/", "", $title));
-            $blog->slug = strtolower(preg_replace("/[\s_\']/", "-", $tempSlug));
-        } else {
-            $blog->slug = strtolower(Input::get('blogurl'));
-        }
-
-        //check if the file isset
-        if( Input::hasFile('blogheaderimageedit'))
-        {
-            $image  =   Input::file('blogheaderimageedit');
-            $imagename = md5(date('YmdHis')).'.jpg';
-            $filename = $dir.$imagename;
-
-            if ($image->getMimeType() == 'image/png'
-                || $image->getMimeType() == 'image/jpg'
-                || $image->getMimeType() == 'image/gif'
-                || $image->getMimeType() == 'image/jpeg'
-                || $image->getMimeType() == 'image/pjpeg')
-            {
-                $image->move($dir, $filename);
-                // unlink(public_path().'/'.$blog->blogheaderimage);
-
-                $blog->blogheaderimage  =   'images/blog/'.$imagename;
-            } else {
-                $blog->blogheaderimage  =   'images/blog/'.$imagename;
-            }
-
-        }
-
-        $blog->save();
-
-//        return stripslashes(Input::get('content'));
-        return Redirect::to('/blog/'.$blog->slug);
+        return Redirect::to(Auth::user()->user()->business->branches()->first().'/blog/'.$blog->slug);
     }
 
     public function blogAjax(){
         if(Request::ajax())
         {
           $id = Input::get('id');
-          $blog =   Blog::findOrFail($id);
-          $blog->body = html_entity_decode(stripcslashes($blog->body));
+          $blog =   $this->getDecodedBlogById($id);
+          
           return $blog;
-        }
-    }
-
-    public function addBlogAjax() {
-        if(Request::ajax())
-        {
-            $blog = New Blog();
-            $blog->title = Input::get('title');
-            $blog->facebook = Input::get('facebook');
-            $blog->google = Input::get('google');
-            $blog->twitter = Input::get('twitter');
-            $blog->linkedin = Input::get('linkedin');
-            $blog->body = Input::get('content');
-            $blog->slug = Input::get('blogurl');
-            $blog->business_id = Auth::user()->user()->business->id;
-            // $blog->business_id = Auth::user()->user()->id;
-
-            
-            //check if the file isset
-            if( Input::hasFile('blogheaderimage'))
-            {
-                $dir = public_path().'/images/blog/';
-                $image  =   Input::file('blogheaderimage');
-                $imagename = md5(date('YmdHis')).'.jpg';
-                $filename = $dir.$imagename;
-
-                if ($image->getMimeType() == 'image/png'
-                    || $image->getMimeType() == 'image/jpg'
-                    || $image->getMimeType() == 'image/gif'
-                    || $image->getMimeType() == 'image/jpeg'
-                    || $image->getMimeType() == 'image/pjpeg')
-                {
-                    $image->move($dir, $filename);
-                    // unlink(public_path().'/'.$blog->blogheaderimage);
-
-                    $blog->blogheaderimage  =   'images/blog/'.$imagename;
-                } else {
-                    $blog->blogheaderimage  =   'images/blog/'.$imagename;
-                }
-
-            }
-
-            $blog->save();
-
-            return Response::json(['status' => 'saved', 'id' => $blog->id, 'title' => Input::get('title')]);
-        }       
-    }
-
-    public function deleteBlogAjax()
-    {
-        if(Request::ajax()){
-            $id = Input::get('id');
-            $blog = Blog::findOrFail($id);
-            $blog->delete();   
-            
-            return Response::json(['status' => 'deleted']);
-        }
-    }
-
-    public function updateBlogAjax()
-    {
-        if(Request::ajax()){
-
-            // files storage folder
-            $dir = public_path().'/images/blog/';
-            $id = Input::get('id');
-            
-            $blog = Blog::findOrFail($id);
-            $blog->title = Input::get('title');
-            $blog->body = Input::get('content');
-            $blog->facebook = Input::get('facebook');
-            $blog->google = Input::get('google');
-            $blog->twitter = Input::get('twitter');
-            $blog->linkedin = Input::get('linkedin');
-            $blog->slug = Input::get('blogurl');
-            // $blog->business_id  =   Auth::user()->user()->business->id;
-            $blog->business_id = Auth::user()->user()->business->id;
-
-            //check if the file isset
-            if( Input::hasFile('blogheaderimageedit'))
-            {
-                $image  =   Input::file('blogheaderimageedit');
-                $imagename = md5(date('YmdHis')).'.jpg';
-                $filename = $dir.$imagename;
-
-                if ($image->getMimeType() == 'image/png'
-                    || $image->getMimeType() == 'image/jpg'
-                    || $image->getMimeType() == 'image/gif'
-                    || $image->getMimeType() == 'image/jpeg'
-                    || $image->getMimeType() == 'image/pjpeg')
-                {
-                    $image->move($dir, $filename);
-                    // unlink(public_path().'/'.$blog->blogheaderimage);
-
-                    $blog->blogheaderimage  =   'images/blog/'.$imagename;
-                } else {
-                    $blog->blogheaderimage  =   'images/blog/'.$imagename;
-                }
-
-            }
-
-            $blog->save();
-            return Response::json(['status' => 'saved', 'id' => $blog->id, 'title' => Input::get('title')]);
         }
     }
 
