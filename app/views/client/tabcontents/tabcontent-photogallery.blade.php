2fd5ab42d07f11121b4b3ffd930a0f58b65aad0c
Zem <lourdrivera123@gmail.com>
2014-07-10 00:24:37 +0800

    added jssor slider

--- app/views/client/tabcontents/tabcontent-photogallery.blade.php
+++ app/views/client/tabcontents/tabcontent-photogallery.blade.php
@@ -1,14 +1,105 @@
 <div id="company-tabs-photogallery" class="company-tabs-content">
 	<div class="blog block">
 		@if( isOwner($branch->business->slug) || isAdmin() )
-		<a id="add_new_blog" class="a-btn button-2-colorful add-coupon">Add Photos</a><br>
-        <div class="content-container" id="add_blog_block" style="display:none">
+		<a id="add_new_photo" class="a-btn button-2-colorful add-coupon">Add Photos</a><br>
+		{{ Form::open([ 'method' => 'post', 'files' => true, 'multiple' => '', 'id' => 'photogalleryform']) }}
+			<input type="file" id="choosefiles" multiple="" style="display:none">
+			<input type="submit" id="addphotosubmit" style="display:none">
+		</form>
+        <div class="content-container" id="add_new_photo_block" style="display:none">
 		    <div class="blog-post block">
 		        <div class="block-title marginize">
-		            <h1>Upload Photos</h1>
+		            <h1>Uploading Photos</h1>
 		        </div>
 		    </div>
 		</div>
         @endif
 	</div>
+    <script type="text/javascript" src="../scripts/jssor.core.js"></script>
+    <script type="text/javascript" src="../scripts/jssor.utils.js"></script>
+    <script type="text/javascript" src="../scripts/jssor.slider.mini.js"></script>
+  
+  <br/>
+  <center>
+    <!-- Jssor Slider Begin -->
+    <div id="slider2_container" style="position: relative; top: 0px; left: 0px; width: 600px; height: 300px; overflow: hidden; ">
+
+        <!-- Loading Screen -->
+        <div u="loading" style="position: absolute; top: 0px; left: 0px;">
+            <div style="filter: alpha(opacity=70); opacity:0.7; position: absolute; display: block;
+                background-color: #000000; top: 0px; left: 0px;width: 100%;height:100%;">
+            </div>
+            <div style="position: absolute; display: block; background: url(../img/loading.gif) no-repeat center center;
+                top: 0px; left: 0px;width: 100%;height:100%;">
+            </div>
+        </div>
+
+        <!-- Slides Container -->
+        <div u="slides" style="cursor: move; position: absolute; left: 0px; top: 0px; width: 600px; height: 300px; overflow: hidden;" id="photoshere">
+            <div>
+                <img u="image" src="../img/photography/002.jpg" />
+                <!-- <img u="thumb" src="../img/photography/thumb-002.jpg" /> -->
+            </div>
+            <div>
+                <img u="image" src="../img/photography/003.jpg" />
+                <!-- <img u="thumb" src="../img/photography/thumb-003.jpg" /> -->
+            </div>
+            <div>
+                <img u="image" src="../img/photography/004.jpg" />
+                <!-- <img u="thumb" src="../img/photography/thumb-004.jpg" /> -->
+            </div>
+            <div>
+                <img u="image" src="../img/photography/005.jpg" />
+                <!-- <img u="thumb" src="../img/photography/thumb-005.jpg" /> -->
+            </div>
+            <div>
+                <img u="image" src="../img/photography/006.jpg" />
+                <!-- <img u="thumb" src="../img/photography/thumb-006.jpg" /> -->
+            </div>
+            <div>
+                <img u="image" src="../img/photography/007.jpg" />
+                <!-- <img u="thumb" src="../img/photography/thumb-007.jpg" /> -->
+            </div>
+            <div>
+                <img u="image" src="../img/photography/008.jpg" />
+                <!-- <img u="thumb" src="../img/photography/thumb-008.jpg" /> -->
+            </div>
+            <div>
+                <img u="image" src="../img/photography/009.jpg" />
+                <!-- <img u="thumb" src="../img/photography/thumb-009.jpg" /> -->
+            </div>
+            <div>
+                <img u="image" src="../img/photography/010.jpg" />
+                <!-- <img u="thumb" src="../img/photography/thumb-010.jpg" /> -->
+            </div>
+            <div>
+                <img u="image" src="../img/photography/011.jpg" />
+                <!-- <img u="thumb" src="../img/photography/thumb-011.jpg" /> -->
+            </div>
+        </div>
+        <!-- Arrow Left -->
+        <!-- <span u="arrowleft" class="jssora02l" style="width: 55px; height: 55px; top: 123px; left: 8px;">
+        </span> -->
+        <!-- Arrow Right -->
+      <!--   <span u="arrowright" class="jssora02r" style="width: 55px; height: 55px; top: 123px; right: 8px">
+        </span> -->
+        <!-- Arrow Navigator Skin End -->
+        
+        <!-- ThumbnailNavigator Skin Begin -->
+        <div u="thumbnavigator" class="jssort03" style="position: absolute; width: 600px; height: 60px; left:0px; bottom: 0px;">
+            <div style=" background-color: #000; filter:alpha(opacity=30); opacity:.3; width: 100%; height:100%;"></div>
+            <div u="slides" style="cursor: move;">
+                <div u="prototype" class="p" style="POSITION: absolute; WIDTH: 62px; HEIGHT: 32px; TOP: 0; LEFT: 0;">
+                    <div class=w><ThumbnailTemplate style=" WIDTH: 100%; HEIGHT: 100%; border: none;position:absolute; TOP: 0; LEFT: 0;"></ThumbnailTemplate></div>
+                    <div class=c style="POSITION: absolute; BACKGROUND-COLOR: #000; TOP: 0; LEFT: 0">
+                    </div>
+                </div>
+            </div>
+            <!-- Thumbnail Item Skin End -->
+        </div>
+        <!-- ThumbnailNavigator Skin End -->
+        <a style="display: none" href="http://www.jssor.com">javascript</a>
+    </div>
+    <!-- Jssor Slider End -->
+    </center>
 </div>
--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1393,3 +1393,159 @@ $(document).ready(function() {
 	$('#cancel_add_blog').click(function(){
 		$('#add_blog_block').css('display', 'none');
 	});
+
+	$('#add_new_photo').click(function(){
+		$('#choosefiles').trigger('click');
+	});
+
+	$('#choosefiles').change(function(){
+		$('#addphotosubmit').trigger('click');
+	});
+
+	$(document).ready(function() {
+
+	// process the form
+	$('#photogalleryform').submit(function(event) {
+		event.preventDefault();
+          var formData = new FormData($('#editBlogForm')[0]);
+          var token = $('#photogalleryform > input[name="_token"]').val();
+          formData.append('_token',token);
+
+               $.ajax({
+                      url: '/uploadphotos',  //Server script to process data
+                      type: 'post',
+                      xhr: function() {  // Custom XMLHttpRequest
+                          var myXhr = $.ajaxSettings.xhr();
+                          if(myXhr.upload){ // Check if upload property exists
+                              myXhr.upload.addEventListener('progress',progressHandler, true); // For handling the progress of the upload
+                          }
+                          return myXhr;
+                      },
+                      //Ajax events
+                      // beforeSend: beforeSendHandler,
+                      success: completeHandler,
+                      error: errorHandler,
+                      // Form data
+                      data: formData,
+                      
+                      //Options to tell jQuery not to process data or worry about content-type.
+                      cache: false,
+                      contentType: false,
+                      processData: false,
+                      dataType 	: 'json', // what type of data do we expect back from the server
+                  		encode	: true
+                  });
+
+          function completeHandler(e)
+          {
+             if(e.status == 'saved'){
+             	$('#editblog').fadeOut(500, 'linear');
+				$('#editblog').prepend('<center><span class="alert alert-success" id="'+str+'" style="font-size:20px;color:green">New Blog Post has been added</span></center><br>');
+				$("#"+str).fadeOut(2000, "linear", function(){
+                	$(this).remove();
+        		});	
+
+        		$("#table-categories tbody").prepend('<tr data-id="'+e.id+'"><td><span class="category-name">'+e.title+'</span></td><td><a href="javascript:void(0)" class="bs-btn btn-info btn-edit-category" onclick="editBlog($(this))" data-id="'+e.id+'">Edit</a> <a href="javascript:void(0)" data-id="'+e.id+'" onclick="deleteBlog($(this))" class="bs-btn btn-danger btn-delete-blog" data-id="'+e.id+'">Delete</a></td></tr>');
+
+        		$('#titleedit').val('');
+				$('#facebookedit').val('');
+				$('#googleedit').val('');
+				$('#twitteredit').val('');
+				$('#linkedinedit').val('');
+				$('#img-render-blogheaderimageedit').attr('src', '');
+				$('#btn-editblog-settings-logo').val('');
+
+			} else {
+				$('#editblog').prepend('<center><span class="alert alert-error" id="'+str+'" style="font-size:20px;color:red">We are having a problem saving your blog</span></center><br>');
+			}
+          }
+           function beforeSendHandler()
+          {
+
+          }
+         
+          function progressHandler(e){
+              if(e.lengthComputable){
+                 $('.meter').width(Math.floor(e.loaded/e.total*100) + '%');
+              }
+          }  
+          function errorHandler(e){
+          	console.log(e);
+          }   
+	});
+
+	
+	$("#logintosendreview").click(function(e){
+		e.preventDefault();
+		jQuery('#login-form').toggle();
+		$("#login-form input[name='email']").focus();
+		jQuery('#register-form').hide();
+	});
+
+
+});
+
+ jQuery(document).ready(function ($) {
+            var options = {
+                $AutoPlay: true,                                    //[Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
+                $AutoPlaySteps: 1,                                  //[Optional] Steps to go for each navigation request (this options applys only when slideshow disabled), the default value is 1
+                $AutoPlayInterval: 4000,                            //[Optional] Interval (in milliseconds) to go for next slide since the previous stopped if the slider is auto playing, default value is 3000
+                $PauseOnHover: 1,                               //[Optional] Whether to pause when mouse over if a slider is auto playing, 0 no pause, 1 pause for desktop, 2 pause for touch device, 3 pause for desktop and touch device, default value is 1
+
+                $ArrowKeyNavigation: true,   			            //[Optional] Allows keyboard (arrow key) navigation or not, default value is false
+                $SlideDuration: 500,                                //[Optional] Specifies default duration (swipe) for slide in milliseconds, default value is 500
+                $MinDragOffsetToSlide: 20,                          //[Optional] Minimum drag offset to trigger slide , default value is 20
+                //$SlideWidth: 600,                                 //[Optional] Width of every slide in pixels, default value is width of 'slides' container
+                //$SlideHeight: 300,                                //[Optional] Height of every slide in pixels, default value is height of 'slides' container
+                $SlideSpacing: 0, 					                //[Optional] Space between each slide in pixels, default value is 0
+                $DisplayPieces: 1,                                  //[Optional] Number of pieces to display (the slideshow would be disabled if the value is set to greater than 1), the default value is 1
+                $ParkingPosition: 0,                                //[Optional] The offset position to park slide (this options applys only when slideshow disabled), default value is 0.
+                $UISearchMode: 1,                                   //[Optional] The way (0 parellel, 1 recursive, default value is 1) to search UI components (slides container, loading screen, navigator container, arrow navigator container, thumbnail navigator container etc).
+                $PlayOrientation: 1,                                //[Optional] Orientation to play slide (for auto play, navigation), 1 horizental, 2 vertical, 5 horizental reverse, 6 vertical reverse, default value is 1
+                $DragOrientation: 3,                                //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0)
+
+                $ArrowNavigatorOptions: {
+                    $Class: $JssorArrowNavigator$,              //[Requried] Class to create arrow navigator instance
+                    $ChanceToShow: 1,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
+                    $Steps: 1                                       //[Optional] Steps to go for each navigation request, default value is 1
+                },
+
+                $ThumbnailNavigatorOptions: {
+                    $Class: $JssorThumbnailNavigator$,              //[Required] Class to create thumbnail navigator instance
+                    $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
+
+                    $ActionMode: 1,                                 //[Optional] 0 None, 1 act by click, 2 act by mouse hover, 3 both, default value is 1
+                    $AutoCenter: 3,                                 //[Optional] Auto center thumbnail items in the thumbnail navigator container, 0 None, 1 Horizontal, 2 Vertical, 3 Both, default value is 3
+                    $Lanes: 1,                                      //[Optional] Specify lanes to arrange thumbnails, default value is 1
+                    $SpacingX: 3,                                   //[Optional] Horizontal space between each thumbnail in pixel, default value is 0
+                    $SpacingY: 3,                                   //[Optional] Vertical space between each thumbnail in pixel, default value is 0
+                    $DisplayPieces: 9,                              //[Optional] Number of pieces to display, default value is 1
+                    $ParkingPosition: 260,                          //[Optional] The offset position to park thumbnail
+                    $Orientation: 1,                                //[Optional] Orientation to arrange thumbnails, 1 horizental, 2 vertical, default value is 1
+                    $DisableDrag: false                            //[Optional] Disable drag or not, default value is false
+                }
+            };
+
+            var jssor_slider2 = new $JssorSlider$("slider2_container", options);
+            //responsive code begin
+            //you can remove responsive code if you don't want the slider scales while window resizes
+            function ScaleSlider() {
+                var parentWidth = jssor_slider2.$Elmt.parentNode.clientWidth;
+                if (parentWidth)
+                    jssor_slider2.$SetScaleWidth(Math.min(parentWidth, 600));
+                else
+                    window.setTimeout(ScaleSlider, 30);
+            }
+
+            ScaleSlider();
+
+            if (!navigator.userAgent.match(/(iPhone|iPod|iPad|BlackBerry|IEMobile)/)) {
+                $(window).bind('resize', ScaleSlider);
+            }
+
+
+            //if (navigator.userAgent.match(/(iPhone|iPod|iPad)/)) {
+            //    $(window).bind("orientationchange", ScaleSlider);
+            //}
+            //responsive code end
+        });
