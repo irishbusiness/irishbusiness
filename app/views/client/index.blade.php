59faa19d1a30ba44b191da00e707aae8206c8879
dexterb2992@gmail.com <dexterb2992@gmail.com>
2014-10-21 22:00:22 +0800

    fix errors in html validator

--- app/views/client/index.blade.php
+++ app/views/client/index.blade.php
@@ -8,7 +8,7 @@
 <div class="content-container container-16">
 	<!-- <br> -->
 	<div class="videoWrapper">
-	<iframe style="width:650px; height=393px;" src="http://www.youtube.com/embed/k8GJ1vRRSPI?vq=hd720&amp;rel=0&amp;autohide=2&amp;modestbranding=1" frameborder="0" allowfullscreen></iframe>
+	<iframe style="width:650px; height=393px; frameborder=0;" src="http://www.youtube.com/embed/k8GJ1vRRSPI?vq=hd720&amp;rel=0&amp;autohide=2&amp;modestbranding=1" allowfullscreen></iframe>
 	</div>
 	</div><!-- end of .content-container -->
 <!-- </div> -->
