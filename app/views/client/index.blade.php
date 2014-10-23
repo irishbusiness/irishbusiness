3e921e494cdd02aa36dba0ba1a83da4f3c9d1a82
dexterb2992@gmail.com <dexterb2992@gmail.com>
2014-10-23 09:13:29 +0800

    added recently added businesses to the bottom a video in index

--- app/views/client/index.blade.php
+++ app/views/client/index.blade.php
@@ -27,7 +27,7 @@
 						{{ decode($recentcompany->name) }}
 					</a>
 					<div class="entry-excerpt">
-						{{ decode( $recentcompany->business_description ) }}
+						{{ decode( Str::limit( $recentcompany->business_description, 255) ) }}
 					</div>
 				</div>
 			@endif	
