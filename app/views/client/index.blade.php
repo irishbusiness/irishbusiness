762e444f63d025361be34e5b9ca8f4a91dcd7fde
dexterb2992@gmail.com <dexterb2992@gmail.com>
2014-10-27 22:07:38 +0800

    make recently added in index 1 coumn

--- app/views/client/index.blade.php
+++ app/views/client/index.blade.php
@@ -10,29 +10,29 @@
 	<div class="videoWrapper">
 		<iframe style="width:650px; height=393px; frameborder=0;" src="http://www.youtube.com/embed/k8GJ1vRRSPI?vq=hd720&amp;rel=0&amp;autohide=2&amp;modestbranding=1" allowfullscreen></iframe>
 	</div>
-	<div class="entries-list">
-		<div class="separator"></div>
-		<div><h3>Recently <span class="text-colorful">Added</span></h3></div><br/>
-		@foreach($recentlyaddedcompany2 as $recentcompany)
-			@if(!empty($recentcompany->branches->first()->id))
-				<div class="content-container container-16">
-					<a href="{{ URL::to($recentcompany->branches->first()->branchslug) }}" class="thumbnail">
-						@if( ($recentcompany->logo == "" || $recentcompany->logo == "images/companylogos/sample_company.jpg") && $recentcompany->profilebanner != "" )
-							<img src="{{ ($recentcompany->profilebanner == '') ? URL::asset('images/image-not-available.png') : URL::asset($recentcompany->profilebanner) }}" alt="" />
-						@else
-							<img src="{{ ($recentcompany->logo == '') ? URL::asset('images/image-not-available.png') : URL::asset($recentcompany->logo) }}" alt="" />
-						@endif
-					</a>
-					<a href="{{ URL::to($recentcompany->branches->first()->branchslug) }}" class="entry-title">
-						{{ decode($recentcompany->name) }}
-					</a>
-					<div class="entry-excerpt">
-						{{ decode( $recentcompany->business_description ) }}
-					</div>
+</div>
+<div class="entries-list">
+	<div class="separator"></div>
+	<div><h3>Recently <span class="text-colorful">Added</span></h3></div><br/>
+	@foreach($recentlyaddedcompany2 as $recentcompany)
+		@if(!empty($recentcompany->branches->first()->id))
+			<div class="content-container container-16">
+				<a href="{{ URL::to($recentcompany->branches->first()->branchslug) }}" class="thumbnail">
+					@if( ($recentcompany->logo == "" || $recentcompany->logo == "images/companylogos/sample_company.jpg") && $recentcompany->profilebanner != "" )
+						<img src="{{ ($recentcompany->profilebanner == '') ? URL::asset('images/image-not-available.png') : URL::asset($recentcompany->profilebanner) }}" alt="" />
+					@else
+						<img src="{{ ($recentcompany->logo == '') ? URL::asset('images/image-not-available.png') : URL::asset($recentcompany->logo) }}" alt="" />
+					@endif
+				</a>
+				<a href="{{ URL::to($recentcompany->branches->first()->branchslug) }}" class="entry-title">
+					{{ decode($recentcompany->name) }}
+				</a>
+				<div class="entry-excerpt">
+					{{ decode( $recentcompany->business_description ) }}
 				</div>
-			@endif	
-		@endforeach
-	</div>
+			</div>
+		@endif	
+	@endforeach
 </div>
 	
 	<!-- end of .content-container -->
